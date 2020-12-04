e1c8dadfd57ca5d9a8e563c374060dea9c07b6a8
Leonardo Almeida <leonardograselalmeida@gmail.com>
2020-12-04 14:50:31 -0300

    fix(WebConfig) remove code not necessary

--- src/main/java/br/com/leonardoalmeida/restwithspringbootudemy/config/WebConfig.java
+++ src/main/java/br/com/leonardoalmeida/restwithspringbootudemy/config/WebConfig.java
@@ -9,13 +9,4 @@ public class WebConfig extends WebMvcConfigurationSupport {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedMethods("GET", "POST", "PATCH", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
-
-    @Override
-    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
-
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
-    }
 }
